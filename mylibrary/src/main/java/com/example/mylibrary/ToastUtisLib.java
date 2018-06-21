c941526f0279429b932be3e60c9fcf181fa4127b
XieWenSheng <xws11264012>
2018-06-21 18:58:14 +0800

    k

---
+++ mylibrary/src/main/java/com/example/mylibrary/ToastUtisLib.java
@@ -0,0 +1,15 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by DELL on 2018/6/21.
+ */
+
+public class ToastUtisLib {
+
+    public void toast(Context context){
+        Toast.makeText(context,"ToastUtisLib",Toast.LENGTH_LONG).show();
+    }
+}
